3911035c25fd0a59db9a6610a03996d88bc81b53
Gabriel F <gabriel.floresguzman@outlook.com>
2024-08-15 13:41:25 -0600

    cambios para la grafica

--- ClienteAdminCineteca/src/app/components/admin/Contabilidad/ventas/ventas.component.ts
+++ ClienteAdminCineteca/src/app/components/admin/Contabilidad/ventas/ventas.component.ts
@@ -1,16 +1,73 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterLink } from '@angular/router';
 import { AdminSidebarComponent } from '../../admin-sidebar/admin-sidebar.component';
+import { Venta } from '../../../../model/venta.model';
+import { ContabilidadService } from '../../../../services/contabilidad.service';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-ventas',
   standalone: true,
-  imports: [RouterModule,CommonModule,
-    RouterLink,AdminSidebarComponent],
+  imports: [RouterModule, CommonModule, RouterLink, AdminSidebarComponent, NgApexchartsModule],
   templateUrl: './ventas.component.html',
-  styleUrl: './ventas.component.css'
+  styleUrls: ['./ventas.component.css']
 })
-export class VentasComponent {
+export class VentasComponent implements OnInit {
+  ventas: Venta[] = [];
 
+  // Variables para la gráfica usando ApexCharts
+  public chartOptions: any;
+
+  constructor(private contabilidadService: ContabilidadService) {
+    this.chartOptions = {
+      series: [],
+      chart: {
+        type: 'pie',
+        height: 350
+      },
+      labels: [],
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    };
+  }
+
+  ngOnInit(): void {
+    this.obtenerVentas();
+  }
+
+  obtenerVentas(): void {
+    this.contabilidadService.obtenerVentas().subscribe((data: Venta[]) => {
+      this.ventas = data;
+      this.actualizarGrafica();
+    });
+  }
+
+  actualizarGrafica(): void {
+    const productoTotales: { [key: string]: number } = {};
+
+    this.ventas.forEach(venta => {
+      venta.items.forEach(item => {
+        const nombreProducto = item.product.nombre;
+        if (productoTotales[nombreProducto]) {
+          productoTotales[nombreProducto] += item.quantity;
+        } else {
+          productoTotales[nombreProducto] = item.quantity;
+        }
+      });
+    });
+
+    // Actualizamos los datos de la gráfica
+    this.chartOptions.labels = Object.keys(productoTotales);
+    this.chartOptions.series = Object.values(productoTotales);
+  }
 }
